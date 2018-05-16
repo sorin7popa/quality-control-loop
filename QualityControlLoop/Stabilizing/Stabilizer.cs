075802c0f890538d1991b3946ac8061ed0511c93
Sorin Popa <sorin.popa@accesa.eu>
2018-05-16 17:21:10 +0300

    updated v parameter for ReferenceHi2

--- QualityControlLoop/Stabilizing/Stabilizer.cs
+++ QualityControlLoop/Stabilizing/Stabilizer.cs
@@ -13,7 +13,7 @@
 
         public bool SystemErrors(double hi2, int intervalCount, out double referenceHi2)
         {
-            var v = intervalCount + _numberOfParameters + 1;
+            var v = intervalCount - _numberOfParameters + 1;
             referenceHi2 = _standardHi2Values.GetValue(v);
             return hi2 > referenceHi2;
         }
